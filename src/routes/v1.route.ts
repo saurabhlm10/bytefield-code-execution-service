281750a8e58b458f82cd1a327cb52795536e6209
saurabhlm10 <saurabhlm10@gmail.com>
2024-04-17 07:35:04 +0530

    feat: added health route

--- src/routes/v1.route.ts
+++ src/routes/v1.route.ts
@@ -3,6 +3,9 @@ import executeCodeRouter from "./execute-code.route";
 
 const v1Router = Router();
 
+v1Router.get("/health", (req, res) =>
+  res.send("Hello From Bytefield Code Execution Service")
+);
 v1Router.use("/execute", executeCodeRouter);
 
 export default v1Router;
